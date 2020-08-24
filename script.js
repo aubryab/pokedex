93954a6581201cd3600c7112df3a5432ba04cd0d
aubryab <aubrybratcher@gmail.com>
2020-08-24 18:32:20 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const pokedex = document.getElementById("pokedex");
+
+const fetchPokemon = async() => {
+
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = data.results.map((result, index) => 
+        ({
+            ...result,
+            id: index + 1,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+            
+
+        }));
+        displayPokemon(pokemon);
+    }
+    
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLstring = pokemon
+    .map
+    (
+        (pokeman) => `
+    <li class="card" onclick="selectPokemon(${pokeman.id})">
+        <img class="card-image" src="${pokeman.image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+    </li>
+    `
+    ).join("");
+    pokedex.innerHTML = pokemonHTMLstring;
+};
+
+const selectPokemon = async(id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokeman = await res.json();
+    displayPopup(pokeman);
+}
+
+
+// Pokemon pop-up
+const displayPopup = (pokeman) => {
+    const type = pokeman.types.map(type => 
+    type.type.name).join(', ');
+    const image = pokeman.sprites['front_default'];
+    const htmlString = `
+    <div class="popup">
+        <button id="closeBtn"
+        onclick="closePopup()">
+        Close</button>
+        <div class="card">
+            <img class="card-image" 
+            src="${image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p>
+            <small>Height: </small>${pokeman.height} 
+            | <small>Weight: </small>${pokeman.weight} 
+            | <small>Type: </small>${type}
+            </p>
+        </div>
+    </div>
+    `;
+    pokedex.innerHTML = htmlString + pokedex.innerHTML;
+    console.log(htmlString);
+};
+
+
+// Close pop-up
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+}
+
+fetchPokemon();
